44afbbd10c8215e54cc057c33b6112a0c62d60e4
Warren Siro <warrensiro@gmail.com>
2024-04-04 17:43:42 +0300

    removed the ehlo method by adding SSL

--- app2.py
+++ app2.py
@@ -3,10 +3,7 @@ import smtplib
 
 EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
 EMAIL_PASSWORD = os.environ.get("EMAIL_PASS")
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.ehlo()
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
 
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
 
@@ -15,4 +12,4 @@ with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
 
     msg = f"Subject: {subject}\n\n{body}"
 
-    smtp.sendmail(EMAIL_ADDRESS, 'yetahac537@adstam.com', msg)
+    smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg)
